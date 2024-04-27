ce36a895f59be2261356ddf195f22084851a9d6a
Bakir Gracić <bakirgdev@gmail.com>
2024-04-27 19:11:10 +0200

    new website setup

---
+++ app/layout.tsx
@@ -0,0 +1,64 @@
+import './global.css'
+import type { Metadata } from 'next'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+import { Navbar } from './components/nav'
+import Footer from './components/footer'
+import { baseUrl } from './sitemap'
+import { GoogleAnalytics } from '@next/third-parties/google'
+
+export const metadata: Metadata = {
+    metadataBase: new URL(baseUrl),
+    title: {
+        default: '/home/bakir',
+        template: '/home/bakir/%s',
+    },
+    description: 'Personal website and blog of Bakir Gracić. Find insightful blogs and links to my social profiles',
+    openGraph: {
+        title: '/home/bakir',
+        description: 'Personal website and blog of Bakir Gracić. Find insightful blogs and links to my social profiles',
+        url: baseUrl,
+        siteName: '/home/bakir',
+        locale: 'en_US',
+        type: 'website',
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    },
+}
+
+const cx = (...classes) => classes.filter(Boolean).join(' ')
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html
+            lang="en"
+            className={cx(
+                'text-black bg-white dark:text-white dark:bg-black',
+                GeistSans.variable,
+                GeistMono.variable
+            )}
+        >
+            <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+                <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+                    <Navbar />
+                    {children}
+                    <Footer />
+                </main>
+            </body>
+            {process.env.GA_ID && <GoogleAnalytics gaId={process.env.GA_ID} />}
+        </html>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+    return (
+        <section>
+            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+                Hello & Welcome! 👋
+            </h1>
+            <p className="mb-4">
+                {`My name's Bakir and I'm full-stack developer. I'm passionate about building software 
+                that is reliable, robust, fast, secure and that solves real-world problems and improves 
+                people's lives white integrating it in business solutions. I'm currently working on a 
+                few projects that I'm excited to share with you soon. In the meantime, feel free to check 
+                out my latest blog posts below and links to my online profiles!`}
+            </p>
+            <div className="my-8">
+                <BlogPosts />
+            </div>
+        </section>
+    )
+}
