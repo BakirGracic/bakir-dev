afee285247f1852303211c52fb2dbb960e27c6eb
BakirGracic <bakirgdev@gmail.com>
2024-06-07 14:29:19 +0000

    grammar error fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
             </h1>
             <p className="mb-4">
                 {`My name's Bakir and I'm full-stack developer. I'm passionate about building software 
-                that is reliable, robust, fast, secure and solves real-world problems while improving 
-                lives and can be integrated it in business solutions. I'm currently working on a 
+                that is reliable, robust, fast, secure and solves real-world problems, while improving 
+                lives and integrating in business solutions. I'm currently working on a 
                 few projects that I'm excited to share with you soon. In the meantime, feel free to check 
                 out my latest blog posts below and links to my online profiles!`}
             </p>
