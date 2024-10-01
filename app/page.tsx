242ad4b0cdd2da5388c45b100a568dbe6a125ff1
BakirGracic <bakirgdev@gmail.com>
2024-10-01 09:37:55 +0200

    new post, pgk update, slight mods

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
-import { BlogPostsNewest } from 'app/components/posts-newest'
+import { BlogPostsNewest } from "app/components/posts-newest";
 
 export default function Page() {
     return (
         <section>
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-                Howdy <div className='inline-block anim-hand-shake'>👋</div>
+                Howdy <div className="inline-block anim-hand-shake">👋</div>
             </h1>
             <p className="mb-4">
-                {`My name's Bakir and I chose to be a developer. I'm passionate about building 
-                  the best software and integrating it in business. I would also like to mention, 
-                  living this life in the best way out there is a priority for me. I'm currently 
-                  working on a few projects that I'm excited to share with you. In the meantime, 
-                  don't waste a second and go check out my latest blog posts and other links about me!`}
+                {`My name's Bakir and I chose to be a software developer. I'm passionate about building the best 
+                  software & integrating it in business. I'd like to add that living life to the fullest is a top 
+                  priority for me. I'm currently working on a few projects that I'm excited to share with you. In 
+                  the meantime, don't waste a second and go check out my latest blog posts and other links about me!`}
             </p>
-            <h2 className="mt-9 text-xl font-medium tracking-tighter">Recent blog posts</h2>
+            <h2 className="mt-9 text-xl font-medium tracking-tighter">
+                Recent blog posts
+            </h2>
             <div className="mb-8 mt-5">
                 <BlogPostsNewest />
             </div>
         </section>
-    )
+    );
 }
