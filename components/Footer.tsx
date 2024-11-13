5dbf70628c9103188824ca2411044100f274908a
BakirGracic <bakirgdev@gmail.com>
2024-11-13 15:14:26 +0100

    fix footer link href typo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,17 @@
 import ArrowLink from "@/components/ArrowLink";
 
 const footerLinks = [
-    {href: "/links", text: "links"},
-    {href: "https://github.com/BakirGracic/bakir-dev", text: "github"},
-    {href: "/sitemap", text: "sitemap"},
-    {href: "/rss", text: "rss"}
+    { href: "/links", text: "links" },
+    { href: "https://github.com/BakirGracic/bakir-dev", text: "github" },
+    { href: "/sitemap.xml", text: "sitemap" },
+    { href: "/rss", text: "rss" },
 ];
 
 export default function Footer() {
     return (
         <footer className="mt-16">
             <ul className="flex flex-col md:flex-row gap-2 md:gap-5">
-                {footerLinks.map(({href, text}, index) => (
+                {footerLinks.map(({ href, text }, index) => (
                     <li key={index}>
                         <ArrowLink
                             href={href}
