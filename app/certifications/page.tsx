7243cd4b0f9c5ac817635b11e7fbe24a59675868
BakirGracic <bakirgdev@gmail.com>
2024-11-28 08:56:57 +0100

    feature: add new layout for certification cards

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
-import Image from "next/image";
-import GitHubFoundations from "@/public/certifications/github_foundations/github-foundations.png";
+import CertificationCard from "@/components/CertificationCard";
 import type { Metadata } from "next";
+import GitHubFoundations from "@/public/certifications/github_foundations/github-foundations.png";
 
 export const metadata: Metadata = {
     title: "Certifications",
@@ -30,28 +29,24 @@ export const metadata: Metadata = {
 
 const certificationLinks = [
     {
+        img: GitHubFoundations,
+        title: "GitHub Foundations",
+        description: "Learn GitHub and Git",
         link: "https://www.credly.com/badges/6e715e2b-cb75-4c1b-9a2f-aa4610e2c8fc/public_url",
-        icon: GitHubFoundations,
-        title: "GitHub",
     },
 ];
 
 export default function Page() {
     return (
-        <section className="grid grid-cols-3 gap-4">
+        <section className="grid grid-cols-1 sm:grid-cols-2 gap-3">
             {certificationLinks.map((item, index) => (
-                <Link
+                <CertificationCard
                     key={index}
-                    href={item.link}
+                    img={item.img}
                     title={item.title}
-                    target="_blank"
-                    className="p-2 cursor-pointer border border-solid border-neutral-600 rounded-xl duration-[250ms] hover:bg-neutral-800 hover:shadow-inner"
-                >
-                    <Image
-                        src={item.icon}
-                        alt={item.title}
-                    />
-                </Link>
+                    description={item.description}
+                    link={item.link}
+                />
             ))}
         </section>
     );
---
+++ components/CertificationCard.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
+import { CheckBadgeIcon } from "@heroicons/react/16/solid";
+
+export default function CertificationCard({ img, title, description, link }: { img: StaticImageData | string; title: string; description: string; link: string }) {
+    return (
+        <div className="border border-neutral-600 rounded-2xl">
+            <Image
+                src={img}
+                alt="How Much I Wasted"
+                className="rounded-t-2xl"
+                width={9999999}
+                height={9999999}
+            />
+            <div className="p-4">
+                <h2 className="font-semibold text-xl mb-2">{title}</h2>
+                <p className="mb-6">{description}</p>
+                <div className="flex items-baseline flex-wrap justify-end gap-3">
+                    <Link
+                        className="px-3 py-2 rounded-lg border border-neutral-600 flex items-center gap-2"
+                        href={link}
+                        target="_blank"
+                    >
+                        <CheckBadgeIcon className="size-4" />
+                        Verify
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
